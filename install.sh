4cc92ce4bd8f546eb7e268c805c2d3a1fae4b95f
D-Bug #3316 <frc3316@gmail.com>
2018-02-19 21:10:37 +0200

    Added service file and install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+local_path="vision.service"
+systemd_path="/etc/systemd/system/vision.service"
+
+if [ ! -f $local_path ]; then
+	echo " [!] Local file missing. Aborting."
+	exit
+fi
+
+if [ -L $systemd_path ]; then
+	echo " [-] Link exists. Overwriting."
+	sudo rm $systemd_path
+fi
+
+echo " [+] Linking service file."
+sudo ln -s $local_path $systemd_path
+
+echo " [+] Reloading systemd daemons"
+sudo systemctl daemon-reload
